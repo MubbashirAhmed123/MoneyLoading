e6a0d327a5a7ffd80e368a46c7c3ce7e2964169d
Mubbashir <mohdmubbashir71@gmail.com>
2022-11-13 19:29:11 +0530

    all files are commited

--- money_loading.js
+++ money_loading.js
@@ -1,4 +1,4 @@
-const inputval = document.getElementById("amount")
+const amount = document.getElementById("amount")
 const content = document.getElementById("content")
 const first = document.getElementById("first")
 const second = document.getElementById("second")
@@ -12,7 +12,7 @@ const successmsg = document.getElementById("successmsg")
 const errormsg = document.getElementById("errormsg")
 
 
-inputval.addEventListener("input", function () {
+amount.addEventListener("input", function () {
     first.innerText = 0;
     second.innerText = 0;
     third.innerText = 0;
@@ -21,70 +21,70 @@ inputval.addEventListener("input", function () {
     six.innerText = 0;
     seven.innerText = 0;
 
-    if (inputval.value % 2000 == 0) {
+    if (amount.value % 2000 == 0) {
         content.innerHTML = ""
 
-        for (let i = 0; i < ((inputval.value) / 2000); i++) {
+        for (let i = 0; i < ((amount.value) / 2000); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs2000.jpg" alt="2000rs" width="200px">
                     <br>`
             first.innerText++;
 
         }
     }
-    else if (inputval.value % 500 == 0) {
+    else if (amount.value % 500 == 0) {
         content.innerHTML = ""
         second.innerText = 0;
-        for (let i = 0; i < Math.floor(((inputval.value) / 500)); i++) {
+        for (let i = 0; i < Math.floor(((amount.value) / 500)); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs500.jpg" alt="500rs" width="200px">
                            <br>`
             second.innerText++;
 
         }
     }
-    else if (inputval.value % 200 == 0) {
+    else if (amount.value % 200 == 0) {
         content.innerHTML = ""
         third.innerText = 0;
-        for (let i = 0; i < ((inputval.value) / 200); i++) {
+        for (let i = 0; i < ((amount.value) / 200); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs200.jpg" alt="200rs" width="200px">
                            <br>`
             third.innerText++;
 
         }
     }
-    else if (inputval.value % 100 == 0) {
+    else if (amount.value % 100 == 0) {
         content.innerHTML = ""
         four.innerText = 0;
-        for (let i = 0; i < ((inputval.value) / 100); i++) {
+        for (let i = 0; i < ((amount.value) / 100); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs100.jpg" alt="100rs" width="200px">
                            <br>`
             four.innerText++;
 
         }
     }
-    else if (inputval.value % 50 == 0) {
+    else if (amount.value % 50 == 0) {
         content.innerHTML = ""
         fifth.innerText = 0;
-        for (let i = 0; i < ((inputval.value) / 50); i++) {
+        for (let i = 0; i < ((amount.value) / 50); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs50.jpg" alt="50rs" width="200px">
                            <br>`
             fifth.innerText++;
 
         }
     }
-    else if (inputval.value % 20 == 0) {
+    else if (amount.value % 20 == 0) {
         content.innerHTML = ""
         six.innerText = 0;
-        for (let i = 0; i < ((inputval.value) / 20); i++) {
+        for (let i = 0; i < ((amount.value) / 20); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs20.jpg" alt="20rs" width="200px">
                            <br>`
             six.innerText++;
 
         }
     }
-    else if (inputval.value % 10 == 0) {
+    else if (amount.value % 10 == 0) {
         content.innerHTML = ""
         seven.innerText = 0;
-        for (let i = 0; i < ((inputval.value) / 10); i++) {
+        for (let i = 0; i < ((amount.value) / 10); i++) {
             content.innerHTML += `<img class="mx-auto" src="./rs10.jpg" alt="10rs" width="200px">
                            <br>`
             seven.innerText++;
@@ -102,14 +102,14 @@ btn.addEventListener("click", function () {
     six.innerText = 0;
     seven.innerText = 0;
 
-    if (!(inputval.value == "")) {
-        successmsg.innerHTML = `<p class="bg-green-500 p-2 text-green-900 text-center font-bold font-serif">you have successfully withdraw ${inputval.value} Rs!</p>
+    if (!(amount.value == "")) {
+        successmsg.innerHTML = `<p class="bg-green-500 p-2 text-green-900 text-center font-bold font-serif">you have successfully withdraw ${amount.value} Rs!</p>
                 `
         setTimeout(() => {
             successmsg.innerHTML = ""
         }, 3000)
         content.innerHTML = ""
-        inputval.value = ""
+        amount.value = ""
 
     } else {
         errormsg.style.display = "block"
